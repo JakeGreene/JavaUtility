c7fb047d0012a11e39892bc24c030265ad5b9b4c
Jake Greene <jakeandrew.greene@gmail.com>
2013-06-08 16:52:08 -0400

    Flatten Nested Iterables
    The Iterables helper class now has the ability to
    turn an Iterable<? extends Iterable<E>> to an
    Iterable<E>

--- src/ca/jakegreene/util/iterable/Iterables.java
+++ src/ca/jakegreene/util/iterable/Iterables.java
@@ -1,6 +1,8 @@
 package ca.jakegreene.util.iterable;
 
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public final class Iterables {
 	private Iterables() {}
@@ -17,4 +19,21 @@ public final class Iterables {
 			return unsafe;
 		}
 	}
+	
+	/**
+	 * Flatten an iterable of iterables into a single iterable.
+	 * @param nestedIterable
+	 * @return
+	 */
+	public static final <E, I extends Iterable<E>> Iterable<E> flatten(Iterable<I> nestedIterable) {
+		// TODO This currently is in O(n). Can be done in O(1) if we create a NestedIterable and a suitable Iterator
+		List<E> flattened = new LinkedList<E>();
+		for (I iterable : nestedIterable) {
+			for (E element : iterable) {
+				flattened.add(element);
+			}
+		}
+		return flattened;
+	}
+	
 }
